e4edff2a7cf65214235b9ea03a96e508f94a7d3a
Daniel Esuola <esuoladaniel002@gmail.com>
2022-12-09 13:22:49 +0100

    files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#CF95FC',
+        'primary-100': '#9D84B8',
+        'gray': '#ABB2BF',
+        'background': '#1E1D22',
+        'white': '#FFFFFF' 
+      }
+    },
+  },
+  plugins: [],
+}
