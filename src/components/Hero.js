8396f5ef4e1a560df73bd62f63f64dd3ff861ae4
Daniel Esuola <esuoladaniel002@gmail.com>
2022-12-10 21:58:20 +0100

    hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import heroImg from "../assets/img/heroImg.png";
+import dots from "../assets/img/dots.png";
 
 function Hero() {
     return (
-        <div className='h-[100vh] flex justify-center items-end bg-gray-100'>
+        <div className='h-screen flex justify-center items-end bg-gray-100'>
             <div className="w-[80%] h-[100%] mx-auto flex items-end relative">
-                <div className="rectangle h-[34.125em] w-[17em] bg-primary-100 top-0 right-20 absolute z-10"></div>
-                <div className="grid grid-cols-2 w-full h-[70%] z-30">
+
+                <div className="rectangle h-screen w-[17em] bg-primary-100 top-0 right-[11em] absolute z-10"></div>
+
+                <div className="grid lg:grid-cols-2 md:grid-cols-1 w-full h-[75%] z-30">
                     <div className="gap-8 flex flex-col">
                         <h1 className='text-6xl'>Esuola Daniel,</h1>
                         <p className='text-gray'>
-                            Software Developer at <Link to='#' className='text-primary'>Edozzier</Link> and Product Designer✨
+                            Software Developer at <a href="https://edozzier.com/" target='_blank' className='text-primary link relative'>Edozzier</a> and Product Designer✨
                         </p>
                         <p className='text-gray'>
                             I'm a creative developer with years of experience in building products and appealing web experiences.
@@ -21,7 +24,8 @@ function Hero() {
                         </p>
                     </div>
                     <div className="relative">
-                        <img src={heroImg} alt="heroImg" className='w-[90%] absolute right-0 bottom-0' />
+                        <img src={heroImg} alt="heroImg" className='w-[90%] absolute right-0 bottom-0 luminousity' />
+                        <img src={dots} alt="heroImg" className='absolute right-0 bottom-[8em]' />
                     </div>
                 </div>
             </div>
