d4e47a2193470d944092e90bb0964eccad11247e
Daniel Esuola <esuoladaniel002@gmail.com>
2022-12-11 23:20:24 +0100

    Skill section

--- src/components/About.js
+++ src/components/About.js
@@ -16,10 +16,10 @@ function About() {
       <img src={rectangle26} alt="rectangle26" className='right-0 absolute' />
       <img src={rectangle25} alt="rectangle26" className='bottom-20 right-0 absolute' />
       <img src={dots1} alt="rectangle26" className='bottom-1/2 absolute z-10' />
+
       <div className="grid md:grid-cols-2 h-full w-[80vw] mx-auto">
         <div>
           <div className="flex flex-col gap-8">
-
             <span className='flex md:text-5xl text-3xl gap-3'>
               <h1 className='text-primary'>#</h1>
               <h2 className='text-white'>passionate <br /> creative <br /> developer</h2>
