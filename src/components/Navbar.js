1745d7ea71228cecdf093c4c465e5b7249357694
Daniel Esuola <esuoladaniel002@gmail.com>
2022-12-12 22:10:59 +0100

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,20 +51,20 @@ export default function Navbar() {
         <div className=" gap-32 flex flex-col px-10 py-20 justify-between h-full bg-background w-[70vw] shadow-2xl">
           <ul className=''>
             <li className='gap-10 flex flex-col text-4xl'>
-              <NavLink activeClassName='active' to='/' className='font-light text-gray hover:text-white navLink relative hover:font-bold transition-all duration-300'>
+              <NavLink activeClassName='active' to='/' className='font-light text-gray hover:text-white navLink relative hover:font-bold transition-all duration-300' onClick={handleNavClose}>
                 <span className='text-primary'>#</span> about
               </NavLink>
-              <NavLink activeClassName='active' to='/m' className='font-light text-gray hover:text-white navLink relative hover:font-bold transition-all duration-300'>
+              <NavLink activeClassName='active' to='/m' className='font-light text-gray hover:text-white navLink relative hover:font-bold transition-all duration-300' onClick={handleNavClose}>
                 <span className='text-primary'>#</span> works
               </NavLink>
-              <NavLink activeClassName='active' to='/c' className='font-light text-gray hover:text-white navLink relative hover:font-bold transition-all duration-300'>
+              <NavLink activeClassName='active' to='/c' className='font-light text-gray hover:text-white navLink relative hover:font-bold transition-all duration-300' onClick={handleNavClose}>
                 <span className='text-primary'>#</span> contact
               </NavLink>
             </li>
           </ul>
 
           <div className='items-center'>
-            <div className="flex flex-row gap-5 text-2xl justify-center">
+            <div className="flex flex-row gap-8 text-2xl justify-center items-center">
               <a href='https://github.com/danielafriheart' target='_blank' rel='noopener noreferrer' className='text-gray hover:text-white transition-all duration-300'><BsGithub size='1.5em' /></a>
 
               <a href='https://www.behance.net/danielAfriheart' target='_blank' rel='noopener noreferrer' className='text-gray hover:text-white transition-all duration-300'><BsBehance size='1.5em' /></a>
