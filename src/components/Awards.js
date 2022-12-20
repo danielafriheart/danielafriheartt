70b4d3015017bf30277c6474491feae2b331028d
Daniel Esuola <esuoladaniel002@gmail.com>
2022-12-20 23:24:37 +0100

    Added awards and recognition  section

---
+++ src/components/Awards.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function Awards() {
+
+    //Recognitions & Awards =======================>>>>>>>>>>>>>>
+    const awards = [
+        {
+            id: 1,
+            year: 2022,
+            title: 'FreeCodeCamp',
+            recognitionFor: 'Responsive Web Design'
+        },
+    ];
+    //=============================================>>>>>>>>>>>>>>
+    return (
+        <>
+            <div className='py-20 relative'>
+                <div className="h-full w-[85vw] sm:w-[95vw] lg:w-[80vw] mx-auto">
+                    <header className='flex md:text-5xl text-3xl gap-3'>
+                        <h1 className='text-primary'>/</h1>
+                        <h2 className='text-white w-full'>awards & recognitions</h2>
+                        <p className='bg-primary w-full h-[1px] my-auto'></p>
+                    </header>
+
+                    <div className="grid md:grid-cols-[20%,80%] ">
+                        <div className=""></div>
+                        <div className="">
+                            <table className='table-auto table w-full mt-36 text-left'>
+                                <thead className='text-white border-b border-primary-100 border-opacity-25'>
+                                    <tr className='text-3xl'>
+                                        <th className='pb-5'>Project</th>
+                                        <th className='pb-5'>Award</th>
+                                        <th className='pb-5'>Year</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        awards.map((recognition, i) => {
+                                            return (
+                                                <tr className="text-white py-5 border-b border-primary-100 border-opacity-25" key={i}>
+                                                    <td className='py-10'><p>{recognition.title}</p></td>
+                                                    <td className='py-10'><p>{recognition.recognitionFor}</p></td>
+                                                    <td className='py-10'><p>{recognition.year}</p></td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import About from '../components/About'
+import Awards from '../components/Awards'
 import Connect from '../components/Connect'
 import Hero from '../components/Hero'
 import Interest from '../components/Interest'
@@ -14,6 +15,7 @@ export default function Home() {
             <Hero />
             <About />
             <Interest />
+            <Awards />
             <Project />
             <Connect />
         </>
