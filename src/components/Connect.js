bbc2f2d99e6788b8c1752c6c2c8e2a02b215630f
Daniel Esuola <esuoladaniel002@gmail.com>
2023-01-19 17:17:32 +0100

    Update

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,6 +1,4 @@
 import React from 'react'
-//Icons =======================>>>>>>>>>
-//=============================>>>>>>>>>
 //Images =======================>>>>>>>>>
 import rectangle26 from "../assets/img/rectangle26.png";
 import rectangle25 from "../assets/img/rectangle25.png";
@@ -11,29 +9,98 @@ import dots1 from "../assets/img/dots1.png";
 export default function Connect() {
 
     return (
-            <section className='lg:h-[65vh] h-full py-20 relative' data-scroll-section>
-                <img src={bgDots} alt="" className='absolute md:bottom-0 bottom-52 md:left-32 opacity-20 z-10' />
-                <img src={rectangle26} alt="rectangle26" className='right-0 absolute opacity-20 z-10' />
-                <img src={rectangle25} alt="rectangle26" className='bottom-20 right-0 absolute opacity-20 z-10' />
-                <img src={dots1} alt="rectangle26" className='bottom-1/2 absolute z-10 opacity-20' />
-                <div className="h-full w-[85vw] sm:w-[95vw] lg:w-[80vw] mx-auto z-20">
-                    <header className='flex md:text-5xl text-3xl gap-3'>
-                        <h1 className='text-primary'>/</h1>
-                        <h2 className='text-white w-full' id='split'>connect with me ✨</h2>
-                        <p className='bg-primary-100 w-40 h-[1px] my-auto'></p>
-                    </header>
-                    <div className="grid md:grid-cols-[1fr,2fr] mt-20 ">
-                        <div>
-                            <p className='text-gray'>
-                                I am open to exploring freelance opportunities, but am also eager to hear about any other creative projects or inquiries you may have. Please feel free to reach out to me with any questions or ideas.
-                            </p>
-                        </div>
+        <section className='lg:h-[65vh] h-full py-20 relative' data-scroll-section>
 
-                        <div className='h-full md:py-0 py-14 flex flex-col'>
-                            <a href='mailto:esuoladaniel002@gmail.com' target="_blank" rel='noopener noreferrer' type='email' className='text-white opacity-60 hover:opacity-80 underline transition-all duration-300 text-5xl lg:text-8xl sm:text-5xl text-right h2'>hello@daniel <br /> esuola.com</a>
-                        </div>
+            <img src={bgDots} alt="" className='absolute md:bottom-0 bottom-52 left-0 opacity-20 z-10' />
+            <img src={rectangle26} alt="rectangle26" className='right-0 absolute opacity-20 z-10' />
+            <img src={rectangle25} alt="rectangle26" className='bottom-20 right-0 absolute opacity-20 z-10' />
+            <img src={dots1} alt="rectangle26" className='bottom-1/2 absolute z-10 opacity-20' />
+
+            <div className="h-full w-[85vw] sm:w-[95vw] lg:w-[80vw] mx-auto z-20">
+
+                <header className='flex md:text-5xl text-3xl gap-3'>
+
+                    <h1 className='text-primary'
+                        data-aos="fade-zoom-in"
+                        data-aos-easing="ease-in-back"
+                        data-aos-delay="100"
+                        data-aos-offset="0"
+                    >
+                        /
+                    </h1>
+
+                    <div className='flex gap-3'>
+                        <h2 className='text-white w-full'
+                            data-aos="fade-in"
+                            data-aos-delay="200"
+                            data-aos-easing="ease-in-sine"
+                        >
+                            connect
+                        </h2>
+
+                        <h2 className='text-white w-full'
+                            data-aos="fade-in"
+                            data-aos-delay="300"
+                            data-aos-easing="ease-in-sine"
+                        >
+                            with
+                        </h2>
+
+                        <h2 className='text-white w-full'
+                            data-aos="fade-in"
+                            data-aos-delay="400"
+                            data-aos-easing="ease-in-sine"
+                        >
+                            me
+                        </h2>
+
+                    </div>
+
+                    <p className='bg-primary-100 w-40 h-[1px] my-auto'
+                        data-aos="fade-in"
+                        data-aos-delay="800"
+                        data-aos-easing="ease-in-sine"
+                    ></p>
+                </header>
+
+                <div className="grid lg:grid-cols-[2fr,1fr] mt-20 ">
+                    <div className='h-full md:py-0 py-14 flex flex-col relative z-20x'>
+
+                        <a
+                            href='mailto:esuoladaniel002@gmail.com'
+                            className='text-[#A5A5A7] hover:text-[#fff] flex flex-col transition-all duration-300'
+                        >
+                            <span
+                                className='underline transition-all duration-300 text-5xl lg:text-8xl sm:text-5xl h2'
+                                data-aos="fade-in"
+                                data-aos-delay="300"
+                                data-aos-easing="ease-in-sine"
+                            >
+                                hello@daniel
+                            </span>
+                            <span
+                                className='underline transition-all duration-300 text-5xl lg:text-8xl sm:text-5xl h2'
+                                data-aos="fade-in"
+                                data-aos-delay="400"
+                                data-aos-easing="ease-in-sine"
+                            >
+                                esuola.com
+                            </span>
+                        </a>
+
+                    </div>
+                    <div>
+                        <p className='text-gray'
+                            data-aos="fade-in"
+                            data-aos-delay="200"
+                            data-aos-easing="ease-in-sine"
+                        >
+                            I'm open to exploring freelance opportunities, but am also eager to hear about any other creative projects or inquiries you may have. Please feel free to reach out to me with any questions or ideas.
+                        </p>
                     </div>
                 </div>
-            </section>
+
+            </div>
+        </section>
     )
 }
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -8,13 +8,45 @@ import { AiOutlineTwitter } from 'react-icons/ai';
 function SideNav() {
     return (
         <div className='fixed z-40 flex-col items-center ml-8 pb-2 px-2 bg-background hidden lg:flex sm:hidden'>
-            <div className='line bg-gray h-[15em] w-1 rounded-full mb-5'></div>
+            <div className='line bg-gray h-[15em] w-1 rounded-full mb-5'
+                data-aos="fade-down"
+                // data-aos-easing="linear"
+                data-aos-delay="1500"
+            ></div>
+
             <div className="flex flex-col gap-5">
-                <a href='https://github.com/danielafriheart' target='_blank' rel='noopener noreferrer' className='text-gray hover:text-white transition-all duration-300'><GitHubIcon size='1.5em' /></a>
+                <a
+                    href='https://github.com/danielafriheart'
+                    target='_blank' rel='noopener noreferrer'
+                    className='text-gray hover:text-white transition-all duration-300'
+                    data-aos="fade-in"
+                    data-aos-delay="1700"
+                    data-aos-easing="ease-in-sine"  
+                >
+                    <GitHubIcon size='1.5em' />
+                </a>
 
-                <a href='https://www.linkedin.com/in/daniel-esuola/' target='_blank' rel='noopener noreferrer' className='text-gray hover:text-white transition-all duration-300'><ImLinkedin2 size='1.5em' /></a>
+                <a
+                    href='https://www.linkedin.com/in/daniel-esuola/'
+                    target='_blank' rel='noopener noreferrer'
+                    className='text-gray hover:text-white transition-all duration-300'
+                    data-aos="fade-in"
+                    data-aos-delay="1800"
+                    data-aos-easing="ease-in-sine"
+                >
+                    <ImLinkedin2 size='1.5em' />
+                </a>
 
-                <a href='https://twitter.com/DanielAfriheart' target='_blank' rel='noopener noreferrer' className='text-gray hover:text-white transition-all duration-300'><AiOutlineTwitter size='1.5em' /></a>
+                <a
+                    href='https://twitter.com/DanielAfriheart'
+                    target='_blank' rel='noopener noreferrer'
+                    className='text-gray hover:text-white transition-all duration-300'
+                    data-aos="fade-in"
+                    data-aos-delay="1900"
+                    data-aos-easing="ease-in-sine"
+                >
+                    <AiOutlineTwitter size='1.5em' />
+                </a>
             </div>
 
         </div>
